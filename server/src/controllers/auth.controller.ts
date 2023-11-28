3954d044c79b9915554a78958d5ebe980931ff95
Peter-TMK <tundepeter01@gmail.com>
2023-11-28 21:30:26 +0100

    code refactor

---
+++ server/src/controllers/auth.controller.ts
@@ -0,0 +1,76 @@
+import express, { Request, Response } from "express";
+import UserModel from "../models/user.model";
+// import shortid from "shortid";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export const registerUser = async (req: Request, res: Response) => {
+  try {
+    const { fullName, email, password } = req.body;
+
+    // checking if user already exist
+    const existingUser = await UserModel.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already exists!" });
+    }
+
+    // hashing the password
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    // creating a new user
+
+    const newUser = new UserModel({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    // saving user details to db
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully!" });
+
+    // login a user
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error!" });
+  }
+};
+
+export const loginUser = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    // checking if user exists
+    const user = await UserModel.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email/password" });
+    }
+
+    // compare passwords
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ message: "Invalid email/paswword" });
+    }
+
+    // generating JWT token
+    const token = jwt.sign(
+      {
+        email: user.email,
+        id: user.id,
+      },
+      process.env.JWT_SECRET || "helloworld",
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({
+      email: user.email,
+      token,
+      expiresIn: `${3600}s`, // 1 hour
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error!" });
+  }
+};
