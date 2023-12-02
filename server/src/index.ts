28efa964b64be4bff97d7854f16182b46eb3bd26
Peter-TMK <tundepeter01@gmail.com>
2023-12-02 16:24:25 +0100

    refactor the db

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import router from "../src/routes/auth.route";
+import dbConnect from "./config/db";
 
 const app = express();
 const PORT = process.env.PORT || 5555;
@@ -10,21 +11,8 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", router);
 
-const DB_URL = "mongodb://localhost:27017/minify";
+dbConnect();
 
-mongoose.connect(DB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  // Add the following line to resolve the TypeScript error
-  // Use `useCreateIndex` instead of `useNewUrlParser`
-  //   useCreateIndex: true,
-} as mongoose.ConnectOptions);
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error: "));
-db.once("open", () => {
-  console.log("MongoDB connected successfully!");
-  app.listen(PORT, () => {
-    console.log(`Server running at port ${PORT}`);
-  });
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
 });
--- server/src/models/url.model.ts
+++ server/src/models/url.model.ts
@@ -1,23 +1,31 @@
-// import mongoose, { model, Document, Schema } from "mongoose";
+import mongoose, { model, Document, Schema } from "mongoose";
 
-// interface URLModel extends Document {
-//   originalURL: string;
-//   shortURL: string;
-//   customAlias?: string;
-//   createdAt: Date;
-//   updatedAt: Date;
-//   clicks: number;
-// }
+interface URLDocument extends Document {
+  urlCode: string;
+  originalLink: string;
+  userId: string;
+  createdAt: Date;
+  updatedAt: Date;
+  //   clicks: number;
+}
 
-// const urlSchema = new Schema<URLModel>({
-//   originalURL: { type: String, required: true },
-//   shortURL: { type: String, unique: true },
-//   customAlias: { type: String, unique: true, sparse: true },
-//   createdAt: { type: Date, default: Date.now },
-//   updatedAt: { type: Date, default: Date.now },
-//   clicks: { type: Number, default: 0 },
-// });
+const urlSchema = new Schema<URLDocument>({
+  urlCode: { type: String, required: true },
+  originalLink: { type: String, unique: true },
+  userId: { type: String, unique: true, sparse: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+  //   clicks: { type: Number, default: 0 },
+});
 
-// const URLModel = model<URLModel>("Url", urlSchema);
+urlSchema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    delete ret._id;
+  },
+});
 
-// export default URLModel;
+const URLModel = model<URLDocument>("Url", urlSchema);
+
+export default URLModel;
